33cd2b60d9f321df8503207f20a58593269853fd
1996krishna <113167998+1996krishna@users.noreply.github.com>
2022-09-14 11:58:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <div class="w3-bar w3-white w3-wide w3-padding w3-card">
-    <a href="#home" class="w3-bar-item w3-button"><b>BR</b> Architects ( check 1997 )</a>
+    <a href="#home" class="w3-bar-item w3-button"><b>BR</b> Architects ( check 1996 )</a>
     <!-- Float links to the right. Hide them on small screens -->
     <div class="w3-right w3-hide-small">
       <a href="#projects" class="w3-bar-item w3-button">Projects</a>
