f807de3f0c7bea5a5d0ff0e4c9c8180c7e03fc0b
1996krishna <krishan.dabrande@gmail.com>
2022-09-14 10:31:08 +0530

    add css link

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>W3.CSS Template</title>
+<title>MY FIRST STATIC WEB PAGE ON HEROKU</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
 
